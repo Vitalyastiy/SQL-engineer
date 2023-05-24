e1e40884c48786c2a4567874ed740d47cd881975
Vitaly Flerin <118436864+Vitalyastiy@users.noreply.github.com>
2023-05-24 12:09:02 +0300

    Update Macro_tera_exam.sql

--- Macro_tera_exam.sql
+++ Macro_tera_exam.sql
@@ -3,13 +3,43 @@
 --Шаг 2. Пишем макрос, в параметры передаем месяцы
 --Шаг 3. Добавляем значения  (Execute) 
 
+--шаг 1 создаем пустую таблицу со структурой
+--шаг 2 пишем макрос
+--шаг 3 добавляем значения
+
+--drop table subs_all;
+-- delete from subs_all
+create multiset volatile table subs_all ,no log (
+ create_date date format 'yy/mm/dd',
+ cluster_name varchar(50) character set unicode casespecific,
+ macroregion varchar(50) character set unicode casespecific,
+ region varchar(50) character set unicode casespecific,
+ branch_id decimal(4,0),
+ cust_id decimal(12,0),
+ subs_id decimal(12,0),
+ msisdn varchar(20) character set unicode not casespecific,
+ point_name varchar(100) character set unicode not casespecific,
+ nps byteint,
+ nps_key byteint,
+ lt_gr varchar(5) character set unicode not casespecific,
+ age_gr varchar(5) character set unicode not casespecific,
+ gender varchar(3) character set unicode not casespecific,
+ data_gb float,
+ data_gr varchar(9) character set unicode not casespecific,
+ create_dttm timestamp(0),
+ event_date_to_rtk varchar(150) character set unicode casespecific,
+ event_date_instal timestamp(0),
+ alladin_system varchar(50) character set unicode casespecific,
+ last_status_name varchar(100) character set unicode casespecific,
+ reason varchar(50) character set unicode casespecific,
+ diff_day integer)
+primary index (subs_id)
+on commit preserve rows;
+
+--drop table subs_all;
+-- delete from subs_all
 replace macro nps_all (s_nps date, e_nps date, sdate date, edate date) as (
-
-
-replace macro nps_all (s_nps date, e_nps date, sdate date, edate date) as (
-
 --create multiset volatile table subs_all, no log as (
-
 insert into subs_all
 select * from (
 	select
@@ -64,48 +94,50 @@ select * from (
 	from uat_ca.v3_nps_bu a
 	--
 	inner join uat_ca.mc_alladin_subs t1 on a.subs_id = t1.subs_id
-	 and t1.event_date_instal >= date '2022-06-01'
-	 and t1.event_date_instal < date '2022-08-01'
-	-- and t1.event_date_instal >= :sdate
-	-- and t1.event_date_instal < :edate
+	 --and t1.event_date_instal >= date '2022-06-01'
+	 --and t1.event_date_instal < date '2022-08-01'
+	 and t1.event_date_instal >= :sdate
+	 and t1.event_date_instal < :edate
 	where 1=1
-	 and a.create_date >= date '2022-07-01'
-	 and a.create_date < date '2022-08-01'
-	-- and a.create_date >= :s_nps
-	-- and a.create_date < :e_nps
+	-- and a.create_date >= date '2022-07-01'
+	-- and a.create_date < date '2022-08-01'
+	 and a.create_date >= :s_nps
+	 and a.create_date < :e_nps
 ) a
+
 where 1=1 
 ;
-
 --where 1=0 
 --)with data
 --on commit preserve rows;
+);
 
---);
+
 
 --2022
-/*ßíâàðü*/     Execute nps_all (date '2022-01-01', date '2022-02-01', date '2021-12-01', date '2022-02-01');
-/*Ôåâðàëü*/    Execute nps_all (date '2022-02-01', date '2022-03-01', date '2022-01-01', date '2022-03-01');
-/*Ìàðò*/       Execute nps_all (date '2022-03-01', date '2022-04-01', date '2022-02-01', date '2022-04-01');
-/*Àïðåëü*/     Execute nps_all (date '2022-04-01', date '2022-05-01', date '2022-03-01', date '2022-05-01');
-/*Ìàé*/        Execute nps_all (date '2022-05-01', date '2022-06-01', date '2022-04-01', date '2022-06-01');
-/*Èþíü*/       Execute nps_all (date '2022-06-01', date '2022-07-01', date '2022-05-01', date '2022-07-01');
-/*Èþëü*/       Execute nps_all (date '2022-07-01', date '2022-08-01', date '2022-06-01', date '2022-08-01');
-/*Àâãóñò*/     Execute nps_all (date '2022-08-01', date '2022-09-01', date '2022-07-01', date '2022-09-01');
-/*Ñåíòÿáðü*/   Execute nps_all (date '2022-09-01', date '2022-10-01', date '2022-08-01', date '2022-10-01');
-/*Îêòÿáðü*/    Execute nps_all (date '2022-10-01', date '2022-11-01', date '2022-09-01', date '2022-11-01');
-/*Íîÿáðü*/     Execute nps_all (date '2022-11-01', date '2022-12-01', date '2022-10-01', date '2022-12-01');
-/*Äåêàáðü*/    Execute nps_all (date '2022-12-01', date '2023-01-01', date '2022-11-01', date '2023-01-01');
+/*Январь*/     Execute nps_all (date '2022-01-01', date '2022-02-01', date '2021-12-01', date '2022-02-01');
+/*Февраль*/    Execute nps_all (date '2022-02-01', date '2022-03-01', date '2022-01-01', date '2022-03-01');
+/*Март*/       Execute nps_all (date '2022-03-01', date '2022-04-01', date '2022-02-01', date '2022-04-01');
+/*Апрель*/     Execute nps_all (date '2022-04-01', date '2022-05-01', date '2022-03-01', date '2022-05-01');
+/*Май*/        Execute nps_all (date '2022-05-01', date '2022-06-01', date '2022-04-01', date '2022-06-01');
+/*Июнь*/       Execute nps_all (date '2022-06-01', date '2022-07-01', date '2022-05-01', date '2022-07-01');
+/*Июль*/       Execute nps_all (date '2022-07-01', date '2022-08-01', date '2022-06-01', date '2022-08-01');
+/*Август*/     Execute nps_all (date '2022-08-01', date '2022-09-01', date '2022-07-01', date '2022-09-01');
+/*Сентябрь*/   Execute nps_all (date '2022-09-01', date '2022-10-01', date '2022-08-01', date '2022-10-01');
+/*Октябрь*/    Execute nps_all (date '2022-10-01', date '2022-11-01', date '2022-09-01', date '2022-11-01');
+/*Ноябрь*/     Execute nps_all (date '2022-11-01', date '2022-12-01', date '2022-10-01', date '2022-12-01');
+/*Декабрь*/    Execute nps_all (date '2022-12-01', date '2023-01-01', date '2022-11-01', date '2023-01-01');
 
 
 --2023
-/*ßíâàðü*/     Execute nps_all (date '2023-01-01', date '2023-02-01', date '2022-12-01', date '2023-02-01');
-/*Ôåâðàëü*/    Execute nps_all (date '2023-02-01', date '2023-03-01', date '2023-01-01', date '2023-03-01');
-/*Ìàðò*/       Execute nps_all (date '2023-03-01', date '2023-04-01', date '2023-02-01', date '2023-04-01');
-/*Àïðåëü*/     Execute nps_all (date '2023-04-01', date '2023-05-01', date '2023-03-01', date '2023-05-01');
+/*Январь*/     Execute nps_all (date '2023-01-01', date '2023-02-01', date '2022-12-01', date '2023-02-01');
+/*Февраль*/    Execute nps_all (date '2023-02-01', date '2023-03-01', date '2023-01-01', date '2023-03-01');
+/*Март*/       Execute nps_all (date '2023-03-01', date '2023-04-01', date '2023-02-01', date '2023-04-01');
+/*Апрель*/     Execute nps_all (date '2023-04-01', date '2023-05-01', date '2023-03-01', date '2023-05-01');
 -------------------------------------------------
 
 select * from subs_all
+
 
 
 
